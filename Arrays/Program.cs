7354d7430258c2fa477cbf493c59f8a00f4e1470
Dave Bourke <squire40@hotmail.com>
2019-01-26 19:15:04 -0500

    IndexOutOfBoundsException

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,25 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            string[] names = new string[4];
+
+            names[0] = "Shark";
+            names[1] = "Dave";
+            names[2] = "Coco";
+            names[3] = "Buttface";
+
+            Console.WriteLine("Enter your name");
+            Console.WriteLine();
+            string newName = Console.ReadLine();
+
+            names[4] = newName;
+
+            Console.WriteLine();
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"Hi {names[i]}");
+            }
         }
     }
 }
